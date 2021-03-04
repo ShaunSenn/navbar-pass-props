179d21570ee62d747a209f8eadae74aca1461324
Senn <shaunsenn2019@gmail.com>
2021-03-04 15:40:07 -0700

    NavBar-Commit-1

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import NavigationComponent from "./navbar/navbar"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Sup dude?</h1>
+        
+      </div>
+    );
+  }
+}
